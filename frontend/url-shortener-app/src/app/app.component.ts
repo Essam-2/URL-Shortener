daedeb5eee62bc101e50e246d3e2e0f30380209d
Essam <esamanwar2@gmail.com>
2024-02-01 00:33:43 +0300

    [+] call starts-background-component

--- frontend/url-shortener-app/src/app/app.component.ts
+++ frontend/url-shortener-app/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { StarsBackgroundComponent } from './stars-background/stars-background.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, StarsBackgroundComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'url-shortener-app';
