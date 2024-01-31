f11d5ebdd2f70eabe338fcab398188e4c26a331f
Essam <esamanwar2@gmail.com>
2024-02-01 00:33:12 +0300

    [+] add starts-background-component

---
+++ frontend/url-shortener-app/src/app/stars-background/stars-background.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stars-background',
+  standalone: true,
+  imports: [],
+  templateUrl: './stars-background.component.html',
+  styleUrl: './stars-background.component.css',
+})
+export class StarsBackgroundComponent {}
