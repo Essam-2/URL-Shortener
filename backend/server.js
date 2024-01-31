086d0b18ddde2d39905bcbac96c1e90dbf4ead64
Essam <esamanwar2@gmail.com>
2024-01-31 19:39:22 +0300

    [+] post request

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
 const app = express();
 
-app.listen(5000);
+mongoose.connect("mongodb://localhost/urlShortener", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   console.log("here");
 });
+
+app.post("/shortUrl", async (req, res) => {
+  await ShortUrl.create({ fullUrl: req.body.fullUrl });
+
+  res.redirect("/");
+});
+
+app.listen(5000);
