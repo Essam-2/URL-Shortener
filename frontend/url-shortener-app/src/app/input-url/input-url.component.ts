92c673edf94813083171fcb1a8769f6c10d689aa
Essam <esamanwar2@gmail.com>
2024-02-10 23:54:57 +0300

    [+] add function to send the data

--- frontend/url-shortener-app/src/app/input-url/input-url.component.ts
+++ frontend/url-shortener-app/src/app/input-url/input-url.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpService } from '../services/http.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-input-url',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
   templateUrl: './input-url.component.html',
-  styleUrl: './input-url.component.css'
+  styleUrl: './input-url.component.css',
 })
 export class InputUrlComponent {
+  urlValue: string = '';
 
+  constructor(private Http: HttpService) {}
+
+  shortUrl() {
+    this.Http.shortUrl(this.urlValue);
+    this.urlValue = '';
+  }
 }
