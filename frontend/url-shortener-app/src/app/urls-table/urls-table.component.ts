097bb87a420df1d714573b97fbfe32094f57668b
Essam <esamanwar2@gmail.com>
2024-02-11 01:18:27 +0300

    [+] add get all urls functionality

--- frontend/url-shortener-app/src/app/urls-table/urls-table.component.ts
+++ frontend/url-shortener-app/src/app/urls-table/urls-table.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../services/http.service';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-urls-table',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './urls-table.component.html',
-  styleUrl: './urls-table.component.css'
+  styleUrl: './urls-table.component.css',
 })
-export class UrlsTableComponent {
+export class UrlsTableComponent implements OnInit {
+  constructor(private Http: HttpService) {}
 
+  shortUrls: any[] = [];
+
+  ngOnInit() {
+    this.getAllUrls();
+    this.Http.urlAdded.subscribe(() => {
+      this.getAllUrls();
+    });
+  }
+
+  getAllUrls() {
+    this.Http.getShortUrls().subscribe(
+      (response) => {
+        this.shortUrls = response.shortUrls;
+        console.log(this.shortUrls);
+      },
+      (error: any) => {
+        console.error('Error fetching short URLs:', error);
+      }
+    );
+  }
 }
