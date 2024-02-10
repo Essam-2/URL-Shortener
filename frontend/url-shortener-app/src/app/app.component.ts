f7f3413a76febfa4d5cb0cea16caeeac4530bb77
Essam <esamanwar2@gmail.com>
2024-02-10 21:14:09 +0300

    call new components

--- frontend/url-shortener-app/src/app/app.component.ts
+++ frontend/url-shortener-app/src/app/app.component.ts
@@ -2,11 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { StarsBackgroundComponent } from './stars-background/stars-background.component';
+import { InputUrlComponent } from './input-url/input-url.component';
+import { UrlsTableComponent } from './urls-table/urls-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, StarsBackgroundComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    StarsBackgroundComponent,
+    InputUrlComponent,
+    UrlsTableComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
