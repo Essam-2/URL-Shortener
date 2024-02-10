c3190536c2040c7cca7a3575a5412f74ce5c6873
Essam <esamanwar2@gmail.com>
2024-02-10 21:13:58 +0300

    [+] create urls table component

---
+++ frontend/url-shortener-app/src/app/urls-table/urls-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-urls-table',
+  standalone: true,
+  imports: [],
+  templateUrl: './urls-table.component.html',
+  styleUrl: './urls-table.component.css'
+})
+export class UrlsTableComponent {
+
+}
