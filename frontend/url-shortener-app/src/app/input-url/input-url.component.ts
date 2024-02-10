f99a1c53338d8d7e38e17da20c63c4eccfa9cb15
Essam <esamanwar2@gmail.com>
2024-02-10 21:13:37 +0300

    [+] create input url component

---
+++ frontend/url-shortener-app/src/app/input-url/input-url.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-url',
+  standalone: true,
+  imports: [],
+  templateUrl: './input-url.component.html',
+  styleUrl: './input-url.component.css'
+})
+export class InputUrlComponent {
+
+}
