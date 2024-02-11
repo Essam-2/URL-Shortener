66895e08447c5a0f4bccb5285db21fd5732d661c
Essam <esamanwar2@gmail.com>
2024-02-11 06:25:35 +0300

    [+] change the api string

--- frontend/url-shortener-app/src/app/services/http.service.ts
+++ frontend/url-shortener-app/src/app/services/http.service.ts
@@ -1,24 +1,25 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpService {
+  private apiUrl = 'http://localhost:5000';
   constructor(private http: HttpClient) {}
 
   urlAdded: EventEmitter<void> = new EventEmitter<void>();
 
   shortUrl(url: string) {
     this.http
-      .post('http://localhost:5000/shortUrl', { fullUrl: url })
+      .post(`${this.apiUrl}/shortUrl`, { fullUrl: url })
       .subscribe((response) => {
         this.urlAdded.emit();
       });
   }
 
   getShortUrls(): Observable<any> {
-    return this.http.get('http://localhost:5000/');
+    return this.http.get(`${this.apiUrl}/`);
   }
 }
