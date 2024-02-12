77e95fa3a06fa37f9c4a131b65f93b49ba5b0e95
Essam <esamanwar2@gmail.com>
2024-02-12 05:59:32 +0300

    [+] add error and success pops

--- frontend/url-shortener-app/src/app/urls-table/urls-table.component.ts
+++ frontend/url-shortener-app/src/app/urls-table/urls-table.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../services/http.service';
-import { NgFor } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
+import { ErrorPopComponent } from '../error-pop/error-pop.component';
+import { SuccessPopComponent } from '../success-pop/success-pop.component';
 
 @Component({
   selector: 'app-urls-table',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, ErrorPopComponent, NgIf, SuccessPopComponent],
   templateUrl: './urls-table.component.html',
   styleUrl: './urls-table.component.css',
 })
@@ -13,6 +15,10 @@ export class UrlsTableComponent implements OnInit {
   constructor(private Http: HttpService) {}
 
   shortUrls: any[] = [];
+  error: boolean = false;
+  errorMessage: string = '';
+  success: boolean = false;
+  successMessage: string = '';
 
   ngOnInit() {
     this.getAllUrls();
@@ -25,15 +31,25 @@ export class UrlsTableComponent implements OnInit {
   }
 
   getAllUrls() {
-    this.Http.getShortUrls().subscribe(
-      (response) => {
-        this.shortUrls = response.shortUrls;
-        // console.log(this.shortUrls);
-      },
-      (error: any) => {
-        console.error('Error fetching short URLs:', error);
-      }
-    );
+    try {
+      this.Http.getShortUrls().subscribe(
+        (response) => {
+          this.shortUrls = response.shortUrls;
+          // console.log(this.shortUrls);
+        },
+        (error: any) => {
+          console.error('Error fetching short URLs:', error);
+        }
+      );
+    } catch (error) {
+      this.error = true;
+      this.errorMessage = 'Something Went Wrong!';
+    } finally {
+      setTimeout(() => {
+        this.error = false;
+        this.errorMessage = '';
+      }, 2000);
+    }
   }
 
   onShortUrlClick(): void {
@@ -41,15 +57,28 @@ export class UrlsTableComponent implements OnInit {
   }
 
   deleteUrl(id: string) {
-    this.Http.deleteUrl(id).subscribe(
-      () => {
-        console.log('URL deleted successfully');
-        // Optionally, you can perform any additional actions after deletion
-      },
-      (error) => {
-        console.error('Error deleting URL:', error);
-        // Handle error appropriately (e.g., show error message)
-      }
-    );
+    try {
+      this.Http.deleteUrl(id).subscribe(
+        () => {
+          console.log('URL deleted successfully');
+        },
+        (error) => {
+          console.error('Error deleting URL:', error);
+        }
+      );
+      this.success = true;
+      this.successMessage = 'Your URL has been deleted!';
+    } catch (error) {
+      this.error = true;
+      this.success = false;
+      this.errorMessage = 'Something Went Wrong!';
+    } finally {
+      setTimeout(() => {
+        this.error = false;
+        this.success = false;
+        this.errorMessage = '';
+        this.successMessage = '';
+      }, 2000);
+    }
   }
 }
