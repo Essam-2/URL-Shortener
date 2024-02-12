87c2eff99be5005bd6ae3c2824d0532429b094c6
Essam <esamanwar2@gmail.com>
2024-02-12 04:52:30 +0300

    [+] add success pop when short new url

--- frontend/url-shortener-app/src/app/input-url/input-url.component.ts
+++ frontend/url-shortener-app/src/app/input-url/input-url.component.ts
@@ -2,11 +2,13 @@ import { Component, EventEmitter, Inject, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpService } from '../services/http.service';
 import { HttpClientModule } from '@angular/common/http';
+import { SuccessPopComponent } from '../success-pop/success-pop.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-input-url',
   standalone: true,
-  imports: [FormsModule, HttpClientModule],
+  imports: [FormsModule, HttpClientModule, SuccessPopComponent, NgIf],
   templateUrl: './input-url.component.html',
   styleUrl: './input-url.component.css',
 })
@@ -14,12 +16,24 @@ export class InputUrlComponent {
   urlValue: string = '';
 
   constructor(private Http: HttpService) {}
+  success: boolean = false;
 
   @Output() urlAdded: EventEmitter<any> = new EventEmitter<any>();
 
   shortUrl() {
-    this.Http.shortUrl(this.urlValue);
-    this.urlValue = '';
-    this.urlAdded.emit();
+    try {
+      if (this.urlValue) {
+        this.Http.shortUrl(this.urlValue);
+        this.urlValue = '';
+        this.urlAdded.emit();
+        this.success = true;
+      }
+    } catch (error) {
+      this.success = false;
+    } finally {
+      setTimeout(() => {
+        this.success = false;
+      }, 2000);
+    }
   }
 }
