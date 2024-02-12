bf3f51f40b3f63c2570e6b1f88e6dc6ab86644ee
Essam <esamanwar2@gmail.com>
2024-02-12 05:58:25 +0300

    [+] add error pop and make success message dynamic

--- frontend/url-shortener-app/src/app/input-url/input-url.component.ts
+++ frontend/url-shortener-app/src/app/input-url/input-url.component.ts
@@ -4,11 +4,18 @@ import { HttpService } from '../services/http.service';
 import { HttpClientModule } from '@angular/common/http';
 import { SuccessPopComponent } from '../success-pop/success-pop.component';
 import { NgIf } from '@angular/common';
+import { ErrorPopComponent } from '../error-pop/error-pop.component';
 
 @Component({
   selector: 'app-input-url',
   standalone: true,
-  imports: [FormsModule, HttpClientModule, SuccessPopComponent, NgIf],
+  imports: [
+    FormsModule,
+    HttpClientModule,
+    SuccessPopComponent,
+    NgIf,
+    ErrorPopComponent,
+  ],
   templateUrl: './input-url.component.html',
   styleUrl: './input-url.component.css',
 })
@@ -17,22 +24,34 @@ export class InputUrlComponent {
 
   constructor(private Http: HttpService) {}
   success: boolean = false;
+  successMessage: string = '';
+  error: boolean = false;
+  errorMessage: string = '';
 
   @Output() urlAdded: EventEmitter<any> = new EventEmitter<any>();
 
   shortUrl() {
     try {
-      if (this.urlValue) {
-        this.Http.shortUrl(this.urlValue);
-        this.urlValue = '';
-        this.urlAdded.emit();
-        this.success = true;
+      if (!this.urlValue) {
+        this.error = true;
+        this.errorMessage = 'Write a correct URL';
+        return;
       }
+      this.Http.shortUrl(this.urlValue);
+      this.urlValue = '';
+      this.urlAdded.emit();
+      this.success = true;
+      this.successMessage = 'Your URL has been shortened';
     } catch (error) {
       this.success = false;
+      this.error = true;
+      this.errorMessage = 'Something Went Wrong';
     } finally {
       setTimeout(() => {
         this.success = false;
+        this.error = false;
+        this.errorMessage = '';
+        this.successMessage = '';
       }, 2000);
     }
   }
